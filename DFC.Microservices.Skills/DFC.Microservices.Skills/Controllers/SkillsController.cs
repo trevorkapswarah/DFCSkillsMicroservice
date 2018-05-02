5d3518391ba07cf9b5903da443175523eaab9e9e
Trevor Kapswarah <trevk15@yahoo.co.uk>
2018-05-02 13:12:55 +0100

    First check in on Skills service

---
+++ DFC.Microservices.Skills/DFC.Microservices.Skills/Controllers/SkillsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DFC.Microservices.Skills.Controllers
+{
+    [Route("api/[controller]")]
+    public class SkillsController : Controller
+    {
+        // GET api/skills/plumber
+        [HttpGet("{urlname}")]
+        public IDictionary<string, string> Get(string urlName)
+        {
+            var result = new ConcurrentDictionary<string, string>();
+
+            result.TryAdd("HowToBecome", "<div></div>");
+            result.TryAdd("EntryQualifications", "<div></div>");
+            result.TryAdd("WhatYouWillDo", "<div></div>");
+
+            return result;
+        }
+    }
+}
